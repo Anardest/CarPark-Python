3c7b89e7c48408a6a06f8f2b2aad9a79485dfb71
Anardest <associalgamer@gmail.com>
2025-01-18 22:05:48 +0300

    feat: добавлен новый валидатор для проверки наличия в Базе Данных feat: добавлен новый валидатор даты feat: добавлен новый валидатор массива чисел

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,6 @@
 from utils.validate_int import validate_integer
+from utils.record_exists import record_exists
+from utils.validate_date import validate_date_string
+from utils.validate_int import validate_array_integer
 
-__all__ = ["validate_integer"]
+__all__ = ["validate_integer", "validate_date_string", "record_exists","validate_array_integer"]
