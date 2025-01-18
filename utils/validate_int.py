014e6f39705a836c6283c982ee739530587b69c9
Anardest <associalgamer@gmail.com>
2025-01-18 22:06:18 +0300

    feat: добавлен новый валидатор массива чисел

--- utils/validate_int.py
+++ utils/validate_int.py
@@ -1,10 +1,10 @@
 
 def validate_integer(input_value, field_name):
     """
-    Проверяет, можно ли преобразовать входное значение в целое число.
-    Возвращает преобразованное значение или вызывает исключение ValueError.
+    Проводит валидацию значения, проебразуя его в int
     :param input_value: Значение, которое нужно проебразовать
     :param field_name: Имя поля, для отображения в ошибке
+    :return Преобразованное число, если валиден, иначе None
 
     :exception ValueError: Ошибка: поле filed_name должно быть целым числом.
 
@@ -13,4 +13,21 @@ def validate_integer(input_value, field_name):
         return int(input_value)
     except ValueError:
         print(f"Ошибка: поле '{field_name}' должно быть целым числом.")
-        raise  # Прерывает выполнение и передает исключение дальше
+        return None # Прерывает выполнение и передает исключение дальше
+
+def validate_array_integer(input_array):
+    '''
+    Проводит валидацию каждого элемента массива, преобразуя всё в int.
+
+    :param input_array: Список элементов для проверки
+    :return: Преобразованный массив, если все элементы валидны, иначе None
+    '''
+    converted_array = []
+    for element in input_array:
+        try:
+            converted_element = int(element)
+            converted_array.append(converted_element)
+        except ValueError:
+            print(f"Ошибка: элемент '{element}' не является целым числом")
+            return None
+    return converted_array
